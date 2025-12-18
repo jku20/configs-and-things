3d246fe0e6acb944bc3768cc4e03a741f1aa389e
jk2582 <jk2582@cornell.edu>
2025-12-17 23:34:56 -0500

    Add helix config.

--- install-stuff.sh
+++ install-stuff.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Copy files in first while we are still in the dir
+cp -r helix $HOME/.config
+
+# Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
+rustup component add rust-analyzer
+
+# Helix
 mkdir -p $HOME/src
 cd $HOME/src
 git clone https://github.com/helix-editor/helix
