c0e9d8d7b50ce943bf28c803098334e6a76febc0
Jeremy Ku-Benjet <jk2582@en-cc-linux12.coecis.cornell.edu>
2025-12-17 22:59:11 -0500

    Add setup-sh.sh

---
+++ install-stuff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+mkdir -p $HOME/src
+cd $HOME/src
+git clone https://github.com/helix-editor/helix
+cd helix
+cargo install \
+   --profile opt \
+   --config 'build.rustflags="-C target-cpu=native"' \
+   --path helix-term \
+   --locked
+echo 'export HELIX_RUNTIME=~/src/helix/runtime' >> $HOME/.bashrc
